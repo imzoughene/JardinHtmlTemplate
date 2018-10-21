4ab8f5a7386f96a244b1e18098027d06879eaeac
imzoughene <amzian.elyass@gmail.com>
2018-10-21 13:01:07 +0100

    add animation via jquery to Section D

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$( document ).ready(function() {
+    var div_x = $("#div-x")
+    var div_y = $("#div-y")
+    
+    var p1_x  = $("#p1-x")
+    var p2_y  = $("#p2-y")
+    var img1_x  = $("#img-1-x")
+    var img2_y  = $("#img-2-y")
+   
+    div_x.hover(
+        function(){
+            p1_x.css('color', '#5abf28');
+            p2_y.css('color', '#636363');
+            img1_x.attr("src","img/circle-green.jpg");
+            img2_y.attr("src","img/circle-gray.png");
+        }
+    );
+
+    div_y.hover(
+        function(){
+            p2_y.css('color', '#5abf28');
+            p1_x.css('color', '#636363');
+            img2_y.attr("src","img/circle-green.jpg");
+            img1_x.attr("src","img/circle-gray.png");
+        }
+    );
+});
